0cfb2f82c82fb1975a744241acc3597af38f4174
luwam leake <luwamabeb111@gmail.com>
2026-07-10 00:32:56 +0300

    Add project files and VS Code settings

---
+++ day2/python telebirr_tip_calculator.py
@@ -0,0 +1,18 @@
+# Store the bill total and number of people
+bill_total = 1200
+people = 4
+
+# List of names
+names = ["Abel", "Sara", "Mahi", "John"]
+
+# Function
+def split_bill(total, people, tip_rate=0.10):
+    total_with_tip = total + (total * tip_rate)
+    return total_with_tip / people
+
+# Calculate each person's share
+share = split_bill(bill_total, people)
+
+# Print the result
+for name in names:
+    print(f"{name} should pay {share:.2f} ETB")
